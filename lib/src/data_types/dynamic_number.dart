3372729ea5d2382b849633d9d3a1c655124d0fdc
debojyoti452 <debojyoti452@gmail.com>
2022-08-21 01:57:03 +0530

    upgraded

---
+++ lib/src/data_types/dynamic_number.dart
@@ -0,0 +1,20 @@
+class DynamicNumber {
+  DynamicNumber(this.value);
+
+  final num value;
+
+  @override
+  String toString() => '$value';
+
+  @override
+  int get hashCode {
+    return value.hashCode;
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other is! DynamicNumber) return false;
+    return value == other.value;
+  }
+}
